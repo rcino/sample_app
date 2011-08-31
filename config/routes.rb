b3ad42a85ffd6c53b79f87e5de5d8f75230790eb
Ryan Cino <cino.ryan@gmail.com>
2011-08-30 21:31:54 -0400

    Finished Exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
+  #match also creates contact_path and contact_url
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  
   root :to => 'pages#home'
   
   get "pages/contact"
-  
   get "pages/about"
 
   # The priority is based upon order of creation:
